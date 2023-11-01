6932488e4c801cc9a46a467183e650c1ba31bcc9
ReyLeal <reinaldoleal2@gmail.com>
2023-11-01 15:38:09 -0300

    Sync Final

--- views/templates/loginTemplate.php
+++ views/templates/loginTemplate.php
@@ -2,4 +2,5 @@
     <input type="text" name="login" placeholder="login"><br>
     <input type="text" name="senha" placeholder="senha"><br>
     <input type="submit" name="validaLogin" value="Login"><br>
+    <a href="/mvcavancado/login">Recarregar</a>
 </form>
